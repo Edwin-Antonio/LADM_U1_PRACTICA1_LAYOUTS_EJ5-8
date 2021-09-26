a73bdf88912da432c1fe97b08d2ac7d4d0645581
Edwin-Antonio <edanarellanoma@ittepic.edu.mx>
2021-09-25 19:33:23 -0600

    Version 0.0.9

---
+++ app/src/main/java/mx/tecnm/tepic/ladm_u1_practica1_layouts_ej5_8/MainActivity7.kt
@@ -0,0 +1,11 @@
+package mx.tecnm.tepic.ladm_u1_practica1_layouts_ej5_8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity7 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main7)
+    }
+}
